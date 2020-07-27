010d175677cefba620076bf23caa5dd3019d7341
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-27 22:01:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral, :isosceles, :scalene, :sides
+  
+  def initialize(sides)
+    @sides = sides
+  end
+  
+  def kind
+  
+  end
 end
